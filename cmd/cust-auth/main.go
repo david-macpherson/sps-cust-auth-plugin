2a1139f1c75a3e578f16855b27c2600778cbb3fe
David MacPherson <david@tensorworks.com.au>
2024-06-04 05:47:28 +1000

    Added logging

--- cmd/cust-auth/main.go
+++ cmd/cust-auth/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "sps-cust-auth-plugin/pkg/api/server"
+import (
+	"log"
+	"sps-cust-auth-plugin/pkg/api/server"
+)
 
 func main() {
+	log.Default().Println("Starting custom auth plugin")
 	server.Start()
 }
